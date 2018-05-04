943220ffb7ea6cd4536f42a83f16a4e9be7c80fd
Jostein Kringlen <josteinkringlen@gmail.com>
2018-05-04 22:44:04 +0200

    Can now connect and send/receive messages from server

--- src/MessageView.js
+++ src/MessageView.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView} from 'react-native';
+import {StyleSheet, Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView, AsyncStorage} from 'react-native';
 
 import {GiftedChat} from 'react-native-gifted-chat';
-import SocketIOClient from 'socket.io-client';
+import io from 'socket.io-client';
+
+const USER_ID = '@userId';
 
 export default class MessageView extends Component {
 
@@ -10,22 +12,33 @@ export default class MessageView extends Component {
         super();
         this.state = {
             text: 'Enter message',
-            messages: []
+            messages: [],
+            userId: null
         };
 
+        this.determineUser = this.determineUser.bind(this);
+        this.onReceivedMessage = this.onReceivedMessage.bind(this);
         this.onSend = this.onSend.bind(this);
-        this.socket = SocketIOClient('http://localhost:5000')
+        this._storeMessages = this._storeMessages.bind(this);
+
+        this.socket = io.connect('http://lesesalen-chat.herokuapp.com/', {
+            'transports': ['websocket']
+        });
+
+        this.socket.on('on_connect', this.onReceivedMessage);
+        this.socket.on('message', this.onReceivedMessage);
+        this.determineUser();
     }
 
     componentWillMount() {
         this.setState({
             messages: [
                 {
-                    _id: 1,
+                    _id: 1001,
                     text: 'Håper du bruker SVN til dette prosjektet ❤️',
                     createdAt: new Date(),
                     user: {
-                        _id: 2,
+                        _id: 1002,
                         name: 'React Native',
                         avatar: 'http://www.fjellbrass.no/medlemmer/AtleGeitung.JPG',
                     },
@@ -35,23 +48,73 @@ export default class MessageView extends Component {
         })
     }
 
+
+    /**
+     * When a user joins the chatroom, check if they are an existing user.
+     * If they aren't, then ask the server for a userId.
+     * Set the userId to the component's state.
+     */
+    determineUser() {
+        AsyncStorage.getItem(USER_ID)
+            .then((userId) => {
+                // If there isn't a stored userId, then fetch one from the server.
+                if (!userId) {
+                    this.socket.emit('userJoined', null);
+                    this.socket.on('userJoined', (userId) => {
+                        AsyncStorage.setItem(USER_ID, userId);
+                        this.setState({userId});
+                    });
+                } else {
+                    this.socket.emit('userJoined', userId);
+                    this.setState({userId});
+                }
+            })
+            .catch((e) => alert(e));
+    }
+
+    /**
+     * When the server sends a message to this.
+     */
+    onReceivedMessage(messages) {
+        console.log(messages);
+
+        //TODO: Don't hard code
+        let message = {
+            _id: 15,
+            text: messages.data,
+            createdAt: new Date(),
+            user: {
+                _id: 1002,
+                name: 'React Native',
+                avatar: 'http://www.fjellbrass.no/medlemmer/AtleGeitung.JPG',
+            },
+        };
+        this._storeMessages(message);
+    }
+
     onSend(messages = []) {
-        this.setState(previousState => ({
-            messages: GiftedChat.append(previousState.messages, messages),
-        }));
-        alert("hei");
-        this.socket.send('message', 'Hello world!');
+        this.socket.send(messages[0]);
+        this._storeMessages(messages);
+    }
+
+    // Helper functions
+    _storeMessages(messages) {
+        this.setState((previousState) => {
+            return {
+                messages: GiftedChat.append(previousState.messages, messages),
+            };
+        });
     }
 
     render() {
+
+        let user = { _id: this.state.userId || -1 };
         return (
-                <GiftedChat
-                    messages={this.state.messages}
-                    onSend={messages => this.onSend(messages)}
-                    user={{
-                        _id: 1,
-                    }}
-                />
+            <GiftedChat
+                messages={this.state.messages}
+                onSend={this.onSend}
+                user={user}
+            />
         )
     }
 
