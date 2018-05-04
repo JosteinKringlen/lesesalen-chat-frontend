e34f8fe75869e10f14cac88b774ebaf0838a7db1
Jostein Kringlen <josteinkringlen@gmail.com>
2018-05-04 20:43:46 +0200

    stuff

--- src/MessageView.js
+++ src/MessageView.js
@@ -1,31 +1,57 @@
-import React, { Component }from 'react';
-import {StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView} from 'react-native';
+
+import {GiftedChat} from 'react-native-gifted-chat';
+import SocketIOClient from 'socket.io-client';
 
 export default class MessageView extends Component {
 
-    constructor(){
+    constructor() {
         super();
-        this.state={
-            text: 'Enter message'
-        }
+        this.state = {
+            text: 'Enter message',
+            messages: []
+        };
+
+        this.onSend = this.onSend.bind(this);
+        this.socket = SocketIOClient('http://localhost:5000')
     }
 
-    render (){
-        return(
-            <View style={styles.container}>
-                <TextInput
-                    style={styles.textInput}
-                    multiline={true}
-                    numberOfLines={4}
-                    placeholder={this.state.text}
-                    onChangeText={(text) => this.setState({text})}
-                    clearTextOnFocus={true}
-                    autoCapitalize={'sentences'}
+    componentWillMount() {
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: 'Håper du bruker SVN til dette prosjektet ❤️',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                        avatar: 'http://www.fjellbrass.no/medlemmer/AtleGeitung.JPG',
+                    },
+                },
+
+            ],
+        })
+    }
+
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }));
+        alert("hei");
+        this.socket.send('message', 'Hello world!');
+    }
+
+    render() {
+        return (
+                <GiftedChat
+                    messages={this.state.messages}
+                    onSend={messages => this.onSend(messages)}
+                    user={{
+                        _id: 1,
+                    }}
                 />
-                <TouchableOpacity onPress={() => alert(this.state.text)}>
-                    <Text>Send</Text>
-                </TouchableOpacity>
-            </View>
         )
     }
 
@@ -33,16 +59,16 @@ export default class MessageView extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 2,
+        flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'space-around',
+        justifyContent: 'center',
         flexDirection: 'row',
     },
     textInput: {
         height: 40,
         borderColor: 'gray',
         width: '80%',
-        borderWidth:1
+        borderWidth: 1
     }
 });
