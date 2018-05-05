a969671b8ac06b2cf511aa0b189be8197a297254
Jostein Kringlen <josteinkringlen@gmail.com>
2018-05-05 22:16:22 +0200

    Removed unused code

--- src/views/MessageView.js
+++ src/views/MessageView.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, AsyncStorage} from 'react-native';
+import {AsyncStorage} from 'react-native';
 
 import {GiftedChat} from 'react-native-gifted-chat';
 import io from 'socket.io-client';
@@ -15,7 +15,6 @@ export default class MessageView extends Component {
             username: '',
         };
 
-        //this.determineUser = this.determineUser.bind(this);
         this.onReceivedMessage = this.onReceivedMessage.bind(this);
         this.onSend = this.onSend.bind(this);
         this.onReceivedSystemMessage = this.onReceivedSystemMessage.bind(this);
@@ -33,31 +32,7 @@ export default class MessageView extends Component {
                 this.socket.emit('join', {username: this.state.username});
             });
         this.socket.on('system', this.onReceivedSystemMessage);
-        //this.determineUser();
     }
-
-    /**
-     * When a user joins the chatroom, check if they are an existing user.
-     * If they aren't, then ask the server for a userId.
-     * Set the userId to the component's state.
-     */
-    /*determineUser() {
-        AsyncStorage.getItem(USER_ID)
-            .then((userId) => {
-                // If there isn't a stored userId, then fetch one from the server.
-                if (!userId) {
-                    this.socket.emit('userJoined', null);
-                    this.socket.on('userJoined', (userId) => {
-                        AsyncStorage.setItem(USER_ID, userId);
-                        this.setState({userId});
-                    });
-                } else {
-                    this.socket.emit('userJoined', userId);
-                    this.setState({userId});
-                }
-            })
-            .catch((e) => alert(e));
-    }*/
 
     async getUserInfo(){
         try {
